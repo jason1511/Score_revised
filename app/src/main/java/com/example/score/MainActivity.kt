532a2bba599d71a64992f965542b7a3cefab39b4
Jason Leonard <jasonleonard151@gmail.com>
2023-09-07 23:10:03 +0700

    Revision

--- app/src/main/java/com/example/score/MainActivity.kt
+++ app/src/main/java/com/example/score/MainActivity.kt
@@ -7,10 +7,12 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     var logres: Int = 0
-    private var x=0
+    var diceval: Int =0
+    var isRolled: Boolean = false
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -20,6 +22,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val diceroll = findViewById<Button>(R.id.btrandom)
+        val diceplace = findViewById<TextView>(R.id.placeholder)
         val scoreb = findViewById<TextView>(R.id.scoreb)
         val score = findViewById<Button>(R.id.add)
         val steal = findViewById<Button>(R.id.min)
@@ -29,42 +33,55 @@ class MainActivity : AppCompatActivity() {
             logres = savedInstanceState.getInt("count")
             scoreb.text = logres.toString()
             when(scoreb.text.toString().toInt()){
-                in 5..9 -> scoreb.setTextColor(Color.RED)
-                in 10..15 -> scoreb.setTextColor(Color.BLUE)
+                in 0..19 -> scoreb.setTextColor(Color.RED)
+                in 20..20 -> scoreb.setTextColor(Color.BLUE)
                 else -> scoreb.setTextColor(Color.BLACK)
             }
         }else{
             scoreb.text = 0.toString()
             scoreb.setTextColor(Color.BLACK)
         }
+        diceroll.setOnClickListener{
+            if(!isRolled){
+                diceval = Random.nextInt(1,7)
+                diceplace.text = diceval.toString()
+                isRolled = true
+            }
+
+        }
         score.setOnClickListener {
             Log.i("lap", "SCORE!!!")
-            if (scoreb.text.toString().toInt() < 15){
-                scoreb.text = (scoreb.text.toString().toInt() + 1).toString()
-            }
+            isRolled = false
+            scoreb.text = (scoreb.text.toString().toInt() + diceval).toString()
+
             when(scoreb.text.toString().toInt()){
-                in 5..9 -> scoreb.setTextColor(Color.RED)
-                in 10..15 -> scoreb.setTextColor(Color.BLUE)
-                15 -> snd.start()
-                else -> scoreb.setTextColor(Color.BLACK)
+                in 0..19 -> scoreb.setTextColor(Color.BLACK)
+                in 20..20 -> scoreb.setTextColor(Color.GREEN)
+                20 -> snd.start()
+                else -> scoreb.setTextColor(Color.RED)
             }
             logres = scoreb.text.toString().toInt()
         }
         steal.setOnClickListener {
             Log.i("lap", "STEAL!!!")
+            isRolled = false
             if (scoreb.text.toString().toInt() > 0){
-                scoreb.text = (scoreb.text.toString().toInt() - 1).toString()
+                scoreb.text = (scoreb.text.toString().toInt() - diceval).toString()
             }
             when(scoreb.text.toString().toInt()){
-                in 5..9 -> scoreb.setTextColor(Color.RED)
-                in 10..15 -> scoreb.setTextColor(Color.BLUE)
-                15 -> snd.start()
-                else -> scoreb.setTextColor(Color.BLACK)
+                in 0..19 -> scoreb.setTextColor(Color.BLACK)
+                in 20..20 -> scoreb.setTextColor(Color.GREEN)
+                20 -> snd.start()
+                else -> scoreb.setTextColor(Color.RED)
             }
             logres = scoreb.text.toString().toInt()
         }
+
         reset.setOnClickListener {
             Log.i("lap", "RESET!!!")
+            if (logres ==20){
+                isRolled = false
+            }
             scoreb.text = 0.toString()
             logres = scoreb.text.toString().toInt()
             scoreb.setTextColor(Color.BLACK)
